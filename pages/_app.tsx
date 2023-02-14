12ed81a15843298f491f7e7fd6f30105204bce9a
mubasyir19 <m.mubasyir19@gmail.com>
2023-02-14 19:49:39 +0700

    Add icon tab

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,24 @@
 // /* eslint-disable react/jsx-props-no-spreading */
-import '../styles/utilities.css';
-import '../styles/homepage.css';
-import '../styles/detail.css';
-import '../styles/checkout.css';
-import '../styles/complete-checkout.css';
-import '../styles/sign-in.css';
-import '../styles/sign-up.css';
-import '../styles/sign-up-photo.css';
-import '../styles/sign-up-success.css';
-import '../styles/404-not-found.css';
-import '../styles/sidebar.css';
-import '../styles/overview.css';
-import '../styles/transactions.css';
-import '../styles/transactions-detail.css';
-import '../styles/edit-profile.css';
-import '../styles/navbar-log-in.css';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import "../styles/utilities.css";
+import "../styles/homepage.css";
+import "../styles/detail.css";
+import "../styles/checkout.css";
+import "../styles/complete-checkout.css";
+import "../styles/sign-in.css";
+import "../styles/sign-up.css";
+import "../styles/sign-up-photo.css";
+import "../styles/sign-up-success.css";
+import "../styles/404-not-found.css";
+import "../styles/sidebar.css";
+import "../styles/overview.css";
+import "../styles/transactions.css";
+import "../styles/transactions-detail.css";
+import "../styles/edit-profile.css";
+import "../styles/navbar-log-in.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -41,6 +41,9 @@ function MyApp({ Component, pageProps }: AppProps) {
 
         {/* AOS Animation */}
         <link href='https://unpkg.com/aos@2.3.1/dist/aos.css' rel='stylesheet' />
+
+        {/* Icon Tab */}
+        <link rel='icon' href='/icon/logo.svg' />
 
         {/* Call Bootstrap JS */}
         <script
